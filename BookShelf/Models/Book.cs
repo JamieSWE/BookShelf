2ccafc71817745889198e5113713460765fa0cd4
Jamie Shaw <jamieshaw94@gmail.com>
2021-10-31 15:44:02 -0500

    Add project files.

---
+++ BookShelf/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class Book
+    {
+        [Key]
+        [DisplayName("ISBN")]
+        [Required(ErrorMessage ="An ISBN number is required.")]
+        public string BookISBN { get; set; }
+
+        [Required(ErrorMessage ="The book's title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="A published date is required.")]
+        [DisplayName("Publish Date")]
+        public DateTimeOffset PublishDate { get; set; }
+
+        [Required(ErrorMessage ="The book amount is required.")]
+        public int Quantity { get; set; }
+
+    }
+}
