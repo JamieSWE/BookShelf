1a72389968300e0f7d2d6e6f9600d03dfba50024
Jamie Shaw <jamieshaw94@gmail.com>
2021-11-03 14:00:24 -0500

    Added MVC for books

---
+++ BookShelf/Data/BookShelfContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookShelf.Models;
+
+namespace BookShelf.Data
+{
+    public class BookShelfContext : DbContext
+    {
+        public BookShelfContext (DbContextOptions<BookShelfContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookShelf.Models.Book> Book { get; set; }
+    }
+}
